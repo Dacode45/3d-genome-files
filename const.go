39e87b4968c4715510e8375e4684b8ba078f4ff7
David Ayeke <david@Signatures-MacBook-Pro.local>
2018-07-17 16:06:35 -0500

    [General] Wrote script - Can download any level and experiment

---
+++ const.go
@@ -0,0 +1,42 @@
+package main
+
+var chromosomes = []string{
+	"chr1",
+	"chr2",
+	"chr3",
+	"chr4",
+	"chr5",
+	"chr6",
+	"chr7",
+	"chr8",
+	"chr9",
+	"chr10",
+	"chr11",
+	"chr12",
+	"chr13",
+	"chr14",
+	"chr15",
+	"chr16",
+	"chr17",
+	"chr18",
+	"chr19",
+	"chr20",
+	"chr21",
+	"chr22",
+	"chrX",
+}
+
+var experiments = []string{
+	"HeLa",
+	"GM12878",
+	"Mcf7",
+	"LHK0102C",
+	"HEK293T",
+}
+
+var levels = []string{
+	"0",
+	"1",
+	"2",
+	"3",
+}
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/urfave/cli"
+)
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func run(c *cli.Context) error {
+	level := c.String("level")
+	if !stringInSlice(level, levels) {
+		return fmt.Errorf("Level %q not valid. Expected one of  %s", level, levels)
+	}
+	experiment := c.String("experiment")
+	if !stringInSlice(experiment, experiments) {
+		return fmt.Errorf("Experiment %q not valid. Expected one of %s", experiment, experiments)
+	}
+
+	dir, err := ioutil.TempDir("", fmt.Sprintf("genome-%s-level_%s", experiment, level))
+	if err != nil {
+		return err
+	}
+
+	for _, chr := range chromosomes {
+		file := filepath.Join(dir, fmt.Sprintf("%s.json", chr))
+		DownloadFile(file, fmt.Sprintf(
+			"http://3dgnome.cent.uw.edu.pl/models/experiment-%s/sample-HCMConverted/%s/level-%s/coords.json",
+			experiment,
+			chr,
+			level,
+		))
+	}
+	log.Printf("Files written to %s\n", dir)
+	return nil
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Action = run
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "level, l",
+			Value: "1",
+			Usage: fmt.Sprintf("Resolution level (one of %s)", levels),
+		},
+
+		cli.StringFlag{
+			Name:  "experiment, e",
+			Value: "GM12878",
+			Usage: fmt.Sprintf("experiment to use (one of %s)", experiments),
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
